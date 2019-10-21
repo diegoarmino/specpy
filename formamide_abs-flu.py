10010dc7df8650c866e692a2e33c8ce8b11901ff
diegoarmino <diegoarmino@gmail.com>
2019-10-21 17:50:13 -0300

    formamide calculations

--- formamide_abs-flu.py
+++ formamide_abs-flu.py
@@ -9,65 +9,40 @@ import sys
 import os.path
 sys.path.append(os.path.abspath('.'))
 sys.path.insert(0, "/home/diegoa/dev/specpy")
-from ramanlib import fluorescence,uvvis,read_table
-##############################################################################
-#     APPLICATIONS: hexatriene
-#     Petrenko & Neese 2007
-##############################################################################
-omega_nm = np.array([354.,444.,934.,1192.,1285.,1295.,1403.,1581.,1635.])
-delta_nm = np.array([0.55,0.23,0.23,0.82,0.485,0.02,0.085,0.38,1.32])
-gamma    = 160.
-omega0=39805
-states_list = np.identity(9)
-states_list.tolist()
-ffreq,fluo = fluorescence(delta_nm,omega_nm,omega0,gamma)
-ufreq,uuvvis = uvvis(delta_nm,omega_nm,omega0,gamma)
-
-
-fig1,ax = plt.subplots(ncols=1,nrows=1,figsize=(8,8))
-xmax=52000
-xmin=28000
-ymax=100
-ymin=0
-plt.xlim(xmin,xmax)
-plt.ylim(ymin,ymax)
-ax.plot(ffreq, fluo)
-ax.plot(ufreq, uuvvis)
-ax.set_xlim(xmin,xmax)
-ax.set_ylim(ymin,ymax)
-ax.set_xlabel(r'$\omega_I$ (cm$^{-1}$)')
-ax.set_aspect((xmax-xmin)/(ymax-ymin),'box')
-
-ax.set_ylabel(r'ABSORPTION/FLUORESCENCE INTENSITY')
-plt.show()
+from ramanlib import fluorescence,uvvis,read_table,uvvis_multi_electronic_state
 
 ##############################################################################
 #  BP86 TZVP PARAMETRS
 ##############################################################################
-delta_nm, omega_nm = read_table('/home/diegoa/dev/specpy/formamide/b3lyp-631/disp.dat')
-gamma    = 800.
-omega0=8065.540107 * 5.8  # Experimental transition for state 1 in eV = 5.8 eV.
+delta_nm, omega_nm = read_table('/home/diegoa/dev/specpy/formamide/b3lyp-631/displacements.dat')
+gamma    = [800.,600.,120.]
+#omega0     = [5.76,7.52,7.99] # in units of eV
+omega0     = [5.76,7.13,8.0] ##,6.17,6.45] # in units of eV
+omega0     = np.asarray(omega0)
+omega0     = omega0*8065.540107 # convert to cm-1
+
+
 #omega0=10**7/218.74
-print(omega0)
 states_list = np.identity(len(omega_nm))
 states_list.tolist()
-ffreq,fluo = fluorescence(delta_nm,omega_nm,omega0,gamma)
-ufreq,uuvvis = uvvis(delta_nm,omega_nm,omega0,gamma)
+#ffreq,fluo = fluorescence(delta_nm,omega_nm,omega0,gamma)
+ufreq,uuvvis =  uvvis_multi_electronic_state(delta_nm,omega_nm,omega0,gamma)
 
 
 fig1,ax = plt.subplots(ncols=1,nrows=1,figsize=(8,8))
-xmax=52000
-xmin=28000
+xmax=10.
+xmin=4.
 ymax=400
 ymin=0
 plt.xlim(xmin,xmax)
-plt.ylim(ymin,ymax)
-ax.plot(ffreq, fluo)
-ax.plot(ufreq, uuvvis)
+#plt.ylim(ymin,ymax)
+#ax.plot(ffreq, fluo)
+ax.plot(ufreq/8065.540107 , uuvvis)
 ax.set_xlim(xmin,xmax)
-ax.set_ylim(ymin,ymax)
-ax.set_xlabel(r'$\omega_I$ (cm$^{-1}$)')
-ax.set_aspect((xmax-xmin)/(ymax-ymin),'box')
+#ax.set_ylim(ymin,ymax)
+#ax.set_xlabel(r'$\omega_I$ (cm$^{-1}$)')
+ax.set_xlabel(r'$\omega_I$ (eV)')
+#ax.set_aspect((xmax-xmin)/(ymax-ymin),'box')
 
 ax.set_ylabel(r'ABSORPTION/FLUORESCENCE INTENSITY')
 plt.show()
@@ -75,7 +50,7 @@ plt.show()
 
 
 fig1,ax = plt.subplots(ncols=1,nrows=1,figsize=(8,8))
-xmax=150
+xmax=100
 xmin=300
 ymax=400
 ymin=0
