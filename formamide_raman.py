0d82185e3b3055fdc647aabb93869c80e238a765
diegoarmino <diegoarmino@gmail.com>
2019-10-24 15:14:12 -0300

    ajkdlfajl

--- formamide_raman.py
+++ formamide_raman.py
@@ -17,23 +17,41 @@ from ramanlib import raman_ex_disp,read_table
 ##############################################################################
 #     RAMAN SHIFT SPECTRA
 ##############################################################################
-delta_nm, omega_nm = read_table('/home/diegoa/dev/specpy/formamide/b3lyp-631/displacements.dat')
+##############################################################################
+#     ALL ELECTRONIC STATES
+##############################################################################
+delta_nm, omega_nm = read_table('/home/diegoa/dev/specpy/formamide/cam-b3lyp-tz/displacements.dat')
 
 ex_lambdas = np.array([192.,200.,209.,218.,229.,252.,325.,407.])
 ex_lambdas = 10**7/ex_lambdas
-print(ex_lambdas)
 #ex_lambdas = np.flip(ex_lambdas,axis=None)
+print(ex_lambdas)
+# [52083 50000. 47846. 45871. 43668. 39682. 30769. 24570.]
+#gamma      = [800.,600.,160.] # 6-31G*
+#gamma      = [500.]*9
+gamma      = [800.]*9
 
-gamma      = 160.
-
-omega0     = [5.76,6.17,6.45] # in units of eV
-omega0     = np.asarray(omega0)
-omega0     = omega0*8065.540107 # convert to cm-1
-
+#omega0     = [5.76,8.0,8.4] # in of eV 6-31G*
+omega0     = [5.6578,6.7266,6.7506,7.2737,7.4984,7.6410,7.8286,8.0887,8.0980]
+omega0     = np.multiply(omega0,8065.540107) # convert to cm-1
 states_list = np.identity(len(omega_nm))
 states_list.tolist()
 
-exmax=55000.
+exmax=90000.
+exmin=20000.
+ex_range = [exmin,exmax]
+sc_range = [0.,4000.]
+gamma_scat = 10.
+
+#states_list = [[1,1,9,1],[4,2],[4,1,9,1],[9,2],[9,2,1,1]]
+
+#omega0     = [5.76,8.0,8.4] # in of eV 6-31G*
+omega0     = [5.6578,6.7266,6.7506,7.2737,7.4984,7.6410,7.8286,8.0887,8.0980]
+omega0     = np.multiply(omega0,8065.540107) # convert to cm-1
+states_list = np.identity(len(omega_nm))
+states_list.tolist()
+print(omega0)
+exmax=90000.
 exmin=20000.
 ex_range = [exmin,exmax]
 sc_range = [0.,4000.]
@@ -41,9 +59,156 @@ gamma_scat = 10.
 
 #states_list = [[1,1,9,1],[4,2],[4,1,9,1],[9,2],[9,2,1,1]]
 states_list = []
+#trans_dip=[0.0076,0.4453/5.,0.0139] # cam-b3lyp/6-31G*
+trans_dip=[0.0013,0.0258,0.0080,0.0995,0.0007,0.1071,0.3443,0.0192,0.0002]
 
-ex_spec_list,freq,spec_array = raman_ex_disp(delta_nm,omega_nm,omega0,gamma,states_list,gamma_scat,sc_range,ex_range,ex_lambdas)
+ex_spec_list,freq,spec_array = raman_ex_disp(delta_nm,omega_nm,omega0,gamma,states_list,gamma_scat,sc_range,ex_range,ex_lambdas,trans_dip)
 
+
+
+
+
+
+##############################################################################
+#    ONLY STATES 1 AND 7
+delta_nm, omega_nm = read_table('/home/diegoa/dev/specpy/formamide/cam-b3lyp-tz/displacements.dat')
+
+ex_lambdas = np.array([192.,200.,209.,218.,229.,10**7/41400.,252.,325.,407.])
+ex_lambdas = 10**7/ex_lambdas
+#ex_lambdas = np.flip(ex_lambdas,axis=None)
+print(ex_lambdas)
+#gamma      = [800.,600.,160.] # 6-31G*
+#gamma      = [500.]*9
+n=5.
+m=1.
+
+gamma      = [n*609./m,n*609.]
+gamma      = [800.,800.]
+#gamma      = [n*609.,n*609./5.]
+
+states_list = np.identity(len(omega_nm))
+states_list.tolist()
+
+#omega0     = [5.76,8.0,8.4] # in of eV 6-31G*
+omega0     = [5.6578,7.82860]
+omega0     = np.multiply(omega0,8065.540107) # convert to cm-1
+omega0     = [39700.,51600.]
+print(np.divide(10**7,omega0))
+
+states_list = np.identity(len(omega_nm))
+states_list.tolist()
+print(omega0)
+exmax=90000.
+exmin=20000.
+ex_range = [exmin,exmax]
+sc_range = [0.,4000.]
+gamma_scat = 10.
+
+#states_list = [[1,1,9,1],[4,2],[4,1,9,1],[9,2],[9,2,1,1]]
+states_list = []
+#trans_dip=[0.0076,0.4453/5.,0.0139] # cam-b3lyp/6-31G*
+trans_dip=[0.3443/200.,0.3443]
+trans_dip=[0.3443,0.3443]
+
+ex_spec_list,freq,spec_array = raman_ex_disp(delta_nm,omega_nm,omega0,gamma,states_list,gamma_scat,sc_range,ex_range,ex_lambdas,trans_dip)
+ex_spec_list,freq,spec_array = raman_ex_disp(delta_nm,omega_nm,[omega0[0]],[gamma[0]],states_list,gamma_scat,sc_range,ex_range,ex_lambdas,[trans_dip[0]])
+ex_spec_list,freq,spec_array = raman_ex_disp(delta_nm,omega_nm,[omega0[1]],[gamma[1]],states_list,gamma_scat,sc_range,ex_range,ex_lambdas,[trans_dip[1]])
+
+
+
+
+
+
+
+
+
+
+##############################################################################
+#    ONLY STATE 1
+delta_nm, omega_nm = read_table('/home/diegoa/dev/specpy/formamide/cam-b3lyp-tz/displacements.dat')
+
+ex_lambdas = np.array([192.,200.,209.,218.,229.,252.,325.,407.])
+ex_lambdas = 10**7/ex_lambdas
+#ex_lambdas = np.flip(ex_lambdas,axis=None)
+
+#gamma      = [800.,600.,160.] # 6-31G*
+#gamma      = [500.]*9
+n=5.
+m=10.
+
+gamma      = [n*609./m]
+#gamma      = [n*609.,n*609./5.]
+
+states_list = np.identity(len(omega_nm))
+states_list.tolist()
+
+#omega0     = [5.76,8.0,8.4] # in of eV 6-31G*
+omega0     = [5.6578]
+omega0     = np.multiply(omega0,8065.540107) # convert to cm-1
+omega0     = [39700.]
+print(np.divide(10**7,omega0))
+
+states_list = np.identity(len(omega_nm))
+states_list.tolist()
+print(omega0)
+exmax=90000.
+exmin=20000.
+ex_range = [exmin,exmax]
+sc_range = [0.,4000.]
+gamma_scat = 10.
+
+#states_list = [[1,1,9,1],[4,2],[4,1,9,1],[9,2],[9,2,1,1]]
+states_list = []
+#trans_dip=[0.0076,0.4453/5.,0.0139] # cam-b3lyp/6-31G*
+trans_dip=[0.3443/200.]
+
+ex_spec_list,freq,spec_array = raman_ex_disp(delta_nm,omega_nm,omega0,gamma,states_list,gamma_scat,sc_range,ex_range,ex_lambdas,trans_dip)
+
+
+
+
+
+
+##############################################################################
+#    ONLY STATE 7
+delta_nm, omega_nm = read_table('/home/diegoa/dev/specpy/formamide/cam-b3lyp-tz/displacements.dat')
+
+ex_lambdas = np.array([192.,200.,209.,218.,229.,252.,325.,407.])
+ex_lambdas = 10**7/ex_lambdas
+#ex_lambdas = np.flip(ex_lambdas,axis=None)
+
+#gamma      = [800.,600.,160.] # 6-31G*
+#gamma      = [500.]*9
+n=5.
+m=10.
+
+gamma      = [n*609.]
+#gamma      = [n*609.,n*609./5.]
+
+states_list = np.identity(len(omega_nm))
+states_list.tolist()
+
+#omega0     = [5.76,8.0,8.4] # in of eV 6-31G*
+omega0     = [7.82860]
+omega0     = np.multiply(omega0,8065.540107) # convert to cm-1
+omega0     = [51600.]
+print(np.divide(10**7,omega0))
+
+states_list = np.identity(len(omega_nm))
+states_list.tolist()
+print(omega0)
+exmax=90000.
+exmin=20000.
+ex_range = [exmin,exmax]
+sc_range = [0.,4000.]
+gamma_scat = 10.
+
+#states_list = [[1,1,9,1],[4,2],[4,1,9,1],[9,2],[9,2,1,1]]
+states_list = []
+#trans_dip=[0.0076,0.4453/5.,0.0139] # cam-b3lyp/6-31G*
+trans_dip=[0.3443]
+
+ex_spec_list,freq,spec_array = raman_ex_disp(delta_nm,omega_nm,omega0,gamma,states_list,gamma_scat,sc_range,ex_range,ex_lambdas,trans_dip)
 
 
 
@@ -56,24 +221,25 @@ fig1,ax = plt.subplots(ncols=1,nrows=len(ex_lambdas),figsize=(16,16),sharex=True
 xmax=4000.
 xmin=0.
 ymin=0.
-idx_v9 = (np.abs(freq - 1635.)).argmin()
+idx_v9 = (np.abs(freq - 1283.)).argmin()
 
-omega_nm   = np.array([354.,444.,934.,1192.,1285.,1295.,1403.,1581.,1635.])
 extra_st = []
 extra_w  = []
-for i in range(len(states_list)):
-    st=[0]*len(omega_nm)
-    for j in range(0,len(states_list[i]),2):
-        st[states_list[i][j]-1]=states_list[i][j+1]
-    extra_st.append(st)
-extra_st = np.asarray(extra_st)
+if len(states_list) > 0:
+    print('caca')
+    for i in range(len(states_list)):
+        st=[0]*len(omega_nm)
+        for j in range(0,len(states_list[i]),2):
+            st[states_list[i][j]-1]=states_list[i][j+1]
+        extra_st.append(st)
+    extra_st = np.asarray(extra_st)
 
-for k in range(len(states_list)):
-    wtmp = sum(extra_st[k]*omega_nm)
-    extra_w.append(wtmp)
-omega_nm = np.append(omega_nm,extra_w,axis=0)
+    for k in range(len(states_list)):
+        wtmp = sum(extra_st[k]*omega_nm)
+        extra_w.append(wtmp)
+    omega_nm = np.append(omega_nm,extra_w,axis=0)
 
-ticks =  np.array([354.,444.,934.,1192.,1295.,1403.,1635.,1989., 2384., 2827., 3270., 3624.])
+ticks =  np.array(omega_nm)
 
 for i in range(len(ex_lambdas)):
     ymax=np.amax(spec_array[i])
@@ -84,13 +250,13 @@ for i in range(len(ex_lambdas)):
     ax[i].set_ylim(ymin,ymax)
     ax[i].set_yticks([])
     ax[i].set_xticks(ticks)
-    ax[i].set_xticklabels(ticks, rotation=75)
-    ax[i].set_aspect((xmax-xmin)/((ymax-ymin)*8),'box')
+    ax[i].set_xticklabels(ticks.astype(int), rotation=75)
+    ax[i].set_aspect((xmax-xmin)/((ymax-ymin)*5),'box')
     ax[i].grid(True, color='darkgrey', linestyle='-', linewidth=0.5)
-    ax[i].text(500,2*ymax/3,str(ex_lambdas[i]), fontsize=18, horizontalalignment='right', verticalalignment='center')
+    ax[i].text(500,2*ymax/3,str(int(10**7/ex_lambdas[i])), fontsize=18, horizontalalignment='right', verticalalignment='center')
 
 ax[i].set_xlabel(r'$\omega_I$ (cm$^{-1}$)')
-plt.savefig('tht_raman.svg', dpi=300, facecolor='w', edgecolor='w',
+plt.savefig('formamide_raman.svg', dpi=300, facecolor='w', edgecolor='w',
         orientation='portrait', papertype=None, format='svg',
         transparent=True, bbox_inches=None, pad_inches=0.1,
         metadata=None)
@@ -98,26 +264,37 @@ plt.savefig('tht_raman.svg', dpi=300, facecolor='w', edgecolor='w',
 plt.show()
 
 
+
+
+
+
+
+
+
+
+
+
 xmax=exmax
 xmin=exmin
-xmin=45000.
-xmax=55000.
+xmin=41000.
+xmax=42000.
 #ymax=5000
 ymin=0
-fig1,ax = plt.subplots(ncols=3,nrows=3,figsize=(8,8),sharex=True, gridspec_kw={'hspace': 0})
+fig1,ax = plt.subplots(ncols=3,nrows=4,figsize=(8,8),sharex=True, gridspec_kw={'hspace': 0})
 cnt=1
 for i in range(4):
     for j in range(3):
         ax[i,j].plot(ex_spec_list[0], ex_spec_list[cnt])
-        ymax=np.amax(ex_spec_list[cnt])
-        ymax=ymax+ymax/10
+    #    ymax=np.amax(ex_spec_list[cnt])
+    #    ymax=ymax+ymax/10
         ax[i,j].set_xlim(xmin,xmax)
-        ax[i,j].set_ylim(ymin,ymax)
-        ax[i,j].set_aspect((xmax-xmin)/(ymax-ymin),'box')
+    #    ax[i,j].set_ylim(ymin,ymax)
+        ax[i,j].set_yscale("log")
+        #ax[i,j].set_aspect((xmax-xmin)/(ymax-ymin),'box')
 #        ax[i,j].set_ylabel(r' INTENSITY')
         cnt+=1
 ax[i,j].set_xlabel(r'$\omega_I$ (cm$^{-1}$)')
-plt.savefig('tht_excitation.svg', dpi=300, facecolor='w', edgecolor='w',
+plt.savefig('formamide_excitation.svg', dpi=300, facecolor='w', edgecolor='w',
         orientation='portrait', papertype=None, format='svg',
         transparent=True, bbox_inches=None, pad_inches=0.1,
         metadata=None)
--- ramanlib.py
+++ ramanlib.py
@@ -103,7 +103,7 @@ def raman_simple(delta_nm,omega_nm_in,omega0_in,gamma_in,state):
 #    freq = freq / ccmfs
     return freq,spec
 
-def uvvis_multi_electronic_state(delta_nm,omega_nm_in,omega0_in,gamma_in):
+def uvvis_multi_electronic_state(delta_nm,omega_nm_in,omega0_in,gamma_in,trans_dip):
     c = cnst.c
     ccmfs = c*100.0/1e15 # speed of light in cm/fs
 
@@ -119,14 +119,13 @@ def uvvis_multi_electronic_state(delta_nm,omega_nm_in,omega0_in,gamma_in):
 #    dt = 1e-2/np.amax(omega_nm)
     dt = 1e-2/np.amax(omega_nm)
     period = pi2/np.amin(omega_nm)
-    tmax = 5.*period
+    tmax = 10.*period
 
 #    ovlap=np.array([])
     ovlap=[]
 
     t=0.0
     s=0.5*delta_nm**2
-    trans_dip=np.asarray([0.0071,0.0689,0.0107/2.0]) # DAMGER!!!
     print('starting time series...')
     nstep=0
     while (t < tmax):
@@ -258,6 +257,74 @@ def fluorescence(delta_nm,omega_nm_in,omega0_in,gamma_in):
 
     return freq,spec
 
+def raman_amplitude_mes(delta_nm,omega_nm_in,omega0_in,gamma_in,state,trans_dip):
+    c = cnst.c
+    ccmfs = c*100.0/1e15 # speed of light in cm/fs
+
+    omega_nm = omega_nm_in #* ccmfs # convert omega form cm-1 to fs-1
+    omega0   = omega0_in   #* ccmfs # convert to frequency fs-1
+    gamma    = gamma_in    #* ccmfs
+
+    hbar = cnst.hbar
+    pi = mth.pi
+    pi2= 2*pi
+    inv2pi= 1/pi2
+
+#    dt = 1e-2/np.amax(omega_nm)
+    dt = 1e-2/np.amax(omega_nm_in)
+    period = pi2/np.amin(omega_nm)
+    tmax = 50.*period
+
+    ovlap=[]
+
+    print(state)
+    t=0.0
+    s=np.multiply(0.5,np.square(delta_nm))
+    fac = [1.,1.,0.5,1./6.,1./24.,1./120.]
+    print('Starting time series...')
+    nstep=0
+    while (t < tmax):
+        Inm0 = 0.0
+        for j in range(len(omega0)):
+            arg=0.0
+            exterm = 1.0
+            for i in range(len(state)):
+                arg1 = 1.0j * omega_nm[i] * t
+                arg2 = -s[(j,i)] * ( 1.0 - cmth.exp(-arg1))
+                arg  = arg + arg2
+                if state[i] != 0:
+                    exterm = exterm * cmth.sqrt( fac[int(state[i])] ) * ( - delta_nm[(j,i)] * cmth.sqrt(0.5) * ( 1 - cmth.exp(- arg1) ) )**int(state[i])
+            arg0 = 1.0j * omega0[j] * t
+            arg = arg - arg0 - gamma[j] * t
+            Inm0 = Inm0 + trans_dip[j] * cmth.exp(arg) * exterm ## DANGER!!
+        ovlap.append(Inm0)
+        t = t + dt
+        if (nstep%20000==0):
+            print('Done with step ',nstep,'out of ',int(tmax/dt))
+        nstep = nstep + 1
+        if (nstep%(int(tmax/dt)-10)==0):
+            print('LAST STEP')
+#    ovlap = np.array(ovlap)
+    print('computing Fourier Transform')
+
+    n=len(ovlap)
+    #ovlap=([0.0]*n)+ovlap
+    n=len(ovlap)
+    #freq = np.arange(-n/2,n/2)*2*np.pi/(2*tmax)
+    freq = np.arange(-n/2,n/2)*2*np.pi/(tmax)
+    freq = -fftshift(freq)
+
+    #spec = abs(fft(ovlap)*np.sqrt(inv2pi) )**2
+    spec = fft(ovlap)*np.sqrt(inv2pi)
+    plt.plot(freq,np.square(np.absolute(spec)))
+    plt.plot(freq,np.absolute(spec))
+    plt.plot(freq,np.real(spec))
+    plt.plot(freq,np.imag(spec))
+    plt.xlim(30000,80000)
+    #plt.yscale('log')
+    plt.show()
+    return freq,spec
+
 def raman_amplitude(delta_nm,omega_nm_in,omega0_in,gamma_in,state):
     c = cnst.c
     ccmfs = c*100.0/1e15 # speed of light in cm/fs
@@ -325,7 +392,7 @@ def trim_spec(min,max,axis,spec):
     trim_sp = spec[min_idx-1:max_idx+1]
     return trim_ax,trim_sp
 
-def raman_ex_disp(delta_nm,omega_nm_in,omega0_in,gamma_in,states_extra,gamma_scat,sc_range,ex_range,ex_lambdas):
+def raman_ex_disp(delta_nm,omega_nm_in,omega0_in,gamma_in,states_extra,gamma_scat,sc_range,ex_range,ex_lambdas,trans_dip):
     state = np.identity(len(omega_nm_in),dtype=int)
     # ADDITIONAL STATES (NOT FUNDAMENTAL TRANSITIONS) TO COMPUTE
     if (len(states_extra)>0):
@@ -348,87 +415,137 @@ def raman_ex_disp(delta_nm,omega_nm_in,omega0_in,gamma_in,states_extra,gamma_sca
     exlambda_idxs = []
     ex_spectrums  = []
 
-
-    print('Computing Raman intensities for transition to state = ', state[0])
-    # Compute square Raman amplitude (excitation spectrum) for the first
-    # vibrational transition in the list.
-    # Following loop sums Raman amplitudes (complex). At the end it is squared.
-    for i in range(len(omega0_in)):
-        print(delta_nm[i])
-        print('COMPUTING RAMAN AMPLITUDE FOR EXCITED ELECTRONIC STATE ', i)
-        ex_ax,ex_ls = raman_amplitude(delta_nm[i],omega_nm_in,omega0_in[i],gamma_in,state[0])
-        # Excitation spectrum as calculated by the Fourier transform is too big and
-        # most of it has zero intensity, so we must trim it to only excitation
-        # frequencies of interest.
-        tmp_x,tmp_y = trim_spec(ex_range[1],ex_range[0],ex_ax,ex_ls)
-        ex_ax=None
-        ex_ls=None
-        if (i==0):
-            tex_ax = tmp_x
-            tex_ls = tmp_y
-        else:
-            tex_ls = tex_ls + tmp_y
-    # Squaring absolute value of Raman amplitude.
-    tex_ls = abs(tex_ls)**2
-    ex_spectrums.append(tex_ax)
-    ex_spectrums.append(tex_ls)
-
-    # Get the closest indices to the target excitation wavelengths.
-    for lex in ex_lambdas:
-        exlambda_idxs.append(np.abs(lex - tex_ax).argmin())
-
-    # Create an x-axis for the dispersion Raman spectra.
-    sc_ax   = np.arange(sc_range[0],sc_range[1],0.2)
-
-    # Compute lorentz functions for each vibrational transition over such x-axis.
-    lorentz = ((sc_ax - omega_nm_in[0])**2+gamma_scat**2)
-    lorentz = gamma_scat/lorentz
-
-    # Multiply lorentzian by the square of the Raman amplitude and accumulate in
-    # lineshape.
-    for idx in exlambda_idxs:
-        lineshape.append( lorentz * tex_ls[ idx ] )
-    lineshape = np.asarray(lineshape)
-
     # Loop over the rest of vibrational transitions.
-    for i in range(1,len(state)):
+    for i in range(0,len(state)):
+    #for i in range(1,len(state)): # DANGER!!!
         print('Computing Raman intensities for transition to state = ', state[i])
         # Compute square Raman amplitude (excitation spectrum) for the first
         # vibrational transition in the list.
         # Following loop sums Raman amplitudes (complex). At the end it is squared.
-        for j in range(len(omega0_in)):
-            print('COMPUTING RAMAN AMPLITUDE FOR EXCITED ELECTRONIC STATE ', j)
-            print(delta_nm[j])
-            ex_ax,ex_ls = raman_amplitude(delta_nm[j],omega_nm_in,omega0_in[j],gamma_in,state[0])
-            # Excitation spectrum as calculated by the Fourier transform is too big and
-            # most of it has zero intensity, so we must trim it to only excitation
-            # frequencies of interest.
-            tmp_x,tmp_y = trim_spec(ex_range[1],ex_range[0],ex_ax,ex_ls)
-            ex_ax=None
-            ex_ls=None
-            if (j==0):
-                tex_ax,tex_ls = tmp_x,tmp_y
-            else:
-                tex_ls = tex_ls + tmp_y
+        ex_ax,ex_ls = raman_amplitude_mes(delta_nm,omega_nm_in,omega0_in,gamma_in,state[i],trans_dip)
+
+        # Excitation spectrum as calculated by the Fourier transform is too big and
+        # most of it has zero intensity, so we must trim it to only excitation
+        # frequencies of interest.
+        tex_ax, tex_ls = trim_spec(ex_range[1], ex_range[0], ex_ax, ex_ls)
+
+        ex_ax = None
+        ex_ls = None
+
         # Squaring absolute value of Raman amplitude.
-        tex_ls = abs(tex_ls)**2
+        tex_ls = np.square(np.absolute(tex_ls))
+
+        # Get the closest indices to the target excitation wavelengths.
+        if i == 0:
+            for lex in ex_lambdas:
+                exlambda_idxs.append(np.abs(lex - tex_ax).argmin())
+            # Create an x-axis for the dispersion Raman spectra.
+            sc_ax = np.arange(sc_range[0],sc_range[1],0.2)
+
+        # Append excitation spectra to list
+        if i == 0: ex_spectrums.append(tex_ax)
         ex_spectrums.append(tex_ls)
 
-        ex_ax=None
-        ex_ls=None
-
         # Compute lorentzian functions for each vibrational transition over such x-axis.
-        lorentz = ((sc_ax - omega_nm_in[i])**2+gamma_scat**2)
-        lorentz = gamma_scat/lorentz
+        lorentz = np.square( np.add( sc_ax , -omega_nm_in[i] ) )
+        lorentz = np.add( lorentz , gamma_scat**2 )
+        lorentz = np.divide( 1.0 , lorentz )
+        lorentz = np.multiply( gamma_scat, lorentz )
 
         # Multiply lorentzian by the square of the Raman amplitude and accumulate in
         # lineshape for each excitation frequency of interest.
         lineshape_tmp = []
         for idx in exlambda_idxs:
-            lineshape_tmp.append( lorentz * tex_ls[ idx ] )
-        lineshape_tmp = np.asarray(lineshape_tmp)
+            lineshape_tmp.append( np.multiply( lorentz , tex_ls[ idx ] ) )
+
         # Accumulate with previous vibrational transitions.
-        lineshape = lineshape + lineshape_tmp
+        if (i == 0):
+            lineshape = lineshape_tmp.copy()
+        else:
+            lineshape = np.add(lineshape , lineshape_tmp)
         lineshape_tmp = None
+
+    print('FINNISHED SPECTRA CALCULATION!!!')
+    return ex_spectrums,sc_ax,lineshape
+    #
+    # a=[1+1j,2+2j,3+3j,4+4j]
+    # b=[5,6,7,8]
+    # print(np.add(a,-1+1j))
+    # print(np.multiply(2.0,np.divide(1.0,b)))
+
+def raman_ex_disp_test(delta_nm,omega_nm_in,omega0_in,gamma_in,states_extra,gamma_scat,sc_range,ex_range,ex_lambdas,trans_dip):
+    state = np.identity(len(omega_nm_in),dtype=int)
+    # ADDITIONAL STATES (NOT FUNDAMENTAL TRANSITIONS) TO COMPUTE
+    if (len(states_extra)>0):
+        extra_st = []
+        extra_w  = []
+        for i in range(len(states_extra)):
+            st=[0]*len(omega_nm_in)
+            for j in range(0,len(states_extra[i]),2):
+                st[states_extra[i][j]-1]=states_extra[i][j+1]
+            extra_st.append(st)
+        extra_st = np.asarray(extra_st)
+        state    = np.append(state,extra_st,axis=0)
+
+        for k in range(len(states_extra)):
+            wtmp = sum(extra_st[k]*omega_nm_in)
+            extra_w.append(wtmp)
+        omega_nm_in = np.append(omega_nm_in,extra_w,axis=0)
+
+    lineshape     = []
+    exlambda_idxs = []
+    ex_spectrums  = []
+
+    # Loop over the rest of vibrational transitions.
+    for i in range(0,len(state)):
+    #for i in range(1,len(state)): # DANGER!!!
+        print('Computing Raman intensities for transition to state = ', state[i])
+        # Compute square Raman amplitude (excitation spectrum) for the first
+        # vibrational transition in the list.
+        # Following loop sums Raman amplitudes (complex). At the end it is squared.
+        for j in range(lent(omega0_in)):
+            ex_ax,ex_ls = raman_amplitude_mes(delta_nm,omega_nm_in,[omega0_in[j]],[gamma_in[j]],state[i],[trans_dip[j]])
+
+            # Excitation spectrum as calculated by the Fourier transform is too big and
+            # most of it has zero intensity, so we must trim it to only excitation
+            # frequencies of interest.
+            tex_ax, tex_ls = trim_spec(ex_range[1], ex_range[0], ex_ax, ex_ls)
+
+            ex_ax = None
+            ex_ls = None
+
+            # Squaring absolute value of Raman amplitude.
+        tex_ls = np.square(np.absolute(tex_ls))
+
+        # Get the closest indices to the target excitation wavelengths.
+        if i == 0:
+            for lex in ex_lambdas:
+                exlambda_idxs.append(np.abs(lex - tex_ax).argmin())
+            # Create an x-axis for the dispersion Raman spectra.
+            sc_ax = np.arange(sc_range[0],sc_range[1],0.2)
+
+        # Append excitation spectra to list
+        if i == 0: ex_spectrums.append(tex_ax)
+        ex_spectrums.append(tex_ls)
+
+        # Compute lorentzian functions for each vibrational transition over such x-axis.
+        lorentz = np.square( np.add( sc_ax , -omega_nm_in[i] ) )
+        lorentz = np.add( lorentz , gamma_scat**2 )
+        lorentz = np.divide( 1.0 , lorentz )
+        lorentz = np.multiply( gamma_scat, lorentz )
+
+        # Multiply lorentzian by the square of the Raman amplitude and accumulate in
+        # lineshape for each excitation frequency of interest.
+        lineshape_tmp = []
+        for idx in exlambda_idxs:
+            lineshape_tmp.append( np.multiply( lorentz , tex_ls[ idx ] ) )
+
+        # Accumulate with previous vibrational transitions.
+        if (i == 0):
+            lineshape = lineshape_tmp.copy()
+        else:
+            lineshape = np.add(lineshape , lineshape_tmp)
+        lineshape_tmp = None
+
     print('FINNISHED SPECTRA CALCULATION!!!')
     return ex_spectrums,sc_ax,lineshape
